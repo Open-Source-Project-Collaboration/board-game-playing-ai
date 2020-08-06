8bd89e62f15252da72159adbc1ae92346e0d716c
Brikaa <40431124+Brikaa@users.noreply.github.com>
2020-08-06 11:12:05 +0200

    Added GameState class
    Edited the code to support piece movements, the game is still static

--- main.py
+++ main.py
@@ -1,66 +1,59 @@
 import pygame
 
 
+class GameState:
+    def __init__(self):
+        self.board = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+                      ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+                      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+
+def load_images():
+    for item in game_state.board:
+        for piece in item:
+            if piece != "--":
+                pieces_images[piece] = pygame.transform.scale(pygame.image.load("Images/" + piece + ".png"),
+                                                              (square_size, square_size))
+
+
 def draw_board():
     for r in range(squares):
         for c in range(squares):
-            # Picks either the white square or the black one
-            color = colors[(r + c) % 2]
-            screen.blit(color, pygame.Rect(r * square_size, c *
-                                           square_size, square_size, square_size))  # Adds the
+            color = colors[(r + c) % 2]  # Picks either the white square or the black one
+            screen.blit(color, pygame.Rect(r * square_size, c * square_size, square_size, square_size))  # Adds the
             # picked square
 
 
-# Images are named in the format 'w' or 'b' for white or black + {the symbol of the piece} + '.png'
-def draw_pieces():
-    for r in [0, 1, 6, 7]:
-        if r == 7:  # First row is white
-            first = "w"
-        elif r == 0:  # Last row is black
-            first = "b"
-
-        elif r in [6, 1]:  # For the rows containing pawns
-            if r == 6:  # 2nd row is white
-                first = "w"
-            else:  # 7th row is black
-                first = "b"
-            piece = pygame.image.load(
-                "Images/" + first + "P.png")  # Loads the pawn image
-            piece_resized = pygame.transform.scale(
-                piece, (square_size, square_size))
-            for c in range(squares):  # Adds the pawn image to each column
-                screen.blit(piece_resized, pygame.Rect(
-                    c * square_size, r * square_size, square_size, square_size))
-            continue
-
-        else:
-            continue
-        # This is reached when r is in [0, 7], ie: the rows containing other pieces than pawns
+def draw_pieces(gs):
+    for r in range(squares):
         for c in range(squares):
-            # Picks the piece from the pieces list according to the column
-            second = pieces[c]
-            piece = pygame.image.load("Images/" + first + second + ".png")
-            piece_resized = pygame.transform.scale(
-                piece, (square_size, square_size))
-            screen.blit(piece_resized, pygame.Rect(c * square_size,
-                                                   r * square_size, square_size, square_size))
+            piece = gs.board[r][c]  # Gets the piece on the square in GameState.board
+            if piece != "--":  # If the square is not empty
+                screen.blit(pieces_images[piece],
+                            pygame.Rect(c * square_size, r * square_size, square_size, square_size))
 
 
 if __name__ == "__main__":
     pygame.init()
     width = height = 512  # Game will run at 512 x 512
     screen = pygame.display.set_mode((width, height))
-    # The black square and the
-    colors = [pygame.image.load("Images/White.png"),
-              pygame.image.load("Images/Black.png")]
+    colors = [pygame.image.load("Images/White.png"), pygame.image.load("Images/Black.png")]  # The black square and the
     # white square images
     squares = 8
     square_size = height // squares
-    pieces = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+    game_state = GameState()
+    pieces_images = {}  # A dictionary with the chess piece notations as keys and the images as values
+    load_images()
     draw_board()
-    draw_pieces()
+    draw_pieces(game_state)
     while True:
         for event in pygame.event.get():  # Checks if the game is still running
             if event.type == pygame.QUIT:
                 exit()
         pygame.display.flip()  # updates the screen
+        draw_pieces(game_state)
